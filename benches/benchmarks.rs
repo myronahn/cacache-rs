5bdddeff57727de33d7964158e243f73eb7b2358
Kat Marchán <kzm@zkat.tech>
2019-07-04 15:51:24 -0700

    feat(write): implement AsyncWriter

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,3 +1,4 @@
+use async_std::task;
 use cacache;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use tempfile;
@@ -12,5 +13,20 @@ fn get(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, get);
+fn async_get(c: &mut Criterion) {
+    let tmp = tempfile::tempdir().unwrap();
+    let cache = tmp.path().to_owned();
+    let data = b"hello world".to_vec();
+    let sri = cacache::put::data(&cache, "hello", data).unwrap();
+    c.bench_function("async_read_hash", move |b| {
+        b.iter(|| {
+            task::block_on(cacache::async_get::read_hash(
+                black_box(&cache),
+                black_box(&sri)
+            ))
+        })
+    });
+}
+
+criterion_group!(benches, get, async_get);
 criterion_main!(benches);
--- src/content/write.rs
+++ src/content/write.rs
@@ -1,7 +1,13 @@
 use std::fs::DirBuilder;
 use std::io::prelude::*;
 use std::path::{Path, PathBuf};
+use std::pin::Pin;
+use std::sync::Mutex;
+use std::task::{Context, Poll};
 
+use async_pool::{Blocking, lazy_blocking, spawn_blocking};
+use async_std::fs as afs;
+use futures::prelude::*;
 use ssri::{Algorithm, Integrity, IntegrityOpts};
 use tempfile::NamedTempFile;
 
@@ -48,6 +54,210 @@ impl Write for Writer {
     fn flush(&mut self) -> std::io::Result<()> {
         self.tmpfile.flush()
     }
+}
+
+pub struct AsyncWriter(Mutex<State>);
+
+enum State {
+    Idle(Option<Inner>),
+    Busy(Blocking<State>),
+}
+
+struct Inner {
+    cache: PathBuf,
+    builder: IntegrityOpts,
+    tmpfile: NamedTempFile,
+    buf: Vec<u8>,
+    last_op: Option<Operation>,
+}
+
+enum Operation {
+    Write(std::io::Result<usize>),
+    Flush(std::io::Result<()>),
+}
+
+impl AsyncWriter {
+    #[allow(clippy::new_ret_no_self)]
+    #[allow(clippy::needless_lifetimes)]
+    pub async fn new(cache: &Path, algo: Algorithm) -> Result<AsyncWriter, Error> {
+        let cache_path = cache.to_path_buf();
+        let mut tmp_path = cache_path.clone();
+        tmp_path.push("tmp");
+        afs::DirBuilder::new().recursive(true).create(&tmp_path).await?;
+        Ok(AsyncWriter(Mutex::new(State::Idle(Some(Inner {
+            cache: cache_path,
+            builder: IntegrityOpts::new().algorithm(algo),
+            tmpfile: lazy_blocking(async move {
+                NamedTempFile::new_in(tmp_path)
+            }).await?,
+            buf: vec![],
+            last_op: None,
+        })))))
+    }
+
+    pub async fn close(self) -> Result<Integrity, Error> {
+        // NOTE: How do I even get access to `inner` safely???
+        // let inner = ???;
+        // Blocking, but should be a very fast op.
+        futures::future::poll_fn(|cx| {
+            let state = &mut *self.0.lock().unwrap();
+
+            loop {
+                match state {
+                    State::Idle(opt) => match opt.take() {
+                        None => return Poll::Ready(None),
+                        Some(inner) => {
+                            let (s, r) = futures::channel::oneshot::channel();
+                            let tmpfile = inner.tmpfile;
+                            let sri = inner.builder.result();
+                            let cpath = path::content_path(&inner.cache, &sri);
+
+                            // Start the operation asynchronously.
+                            *state = State::Busy(spawn_blocking(async move {
+                                let res = afs::DirBuilder::new()
+                                    .recursive(true)
+                                    // Safe unwrap. cpath always has multiple segments
+                                    .create(cpath.parent().unwrap())
+                                    .await
+                                    .map_err(Error::Io);
+                                if res.is_err() {
+                                    let _ = s.send(res.map(|_| sri));
+                                } else {
+                                    let res = tmpfile.persist(cpath);
+                                    let res = res.map_err(Error::PersistError);
+                                    let _ = s.send(res.map(|_| sri));
+                                }
+                                State::Idle(None)
+                            }));
+
+                            return Poll::Ready(Some(r));
+                        }
+                    },
+                    // Poll the asynchronous operation the file is currently blocked on.
+                    State::Busy(task) => *state = futures::ready!(Pin::new(task).poll(cx)),
+                }
+            }
+        })
+        .map(|opt| opt.ok_or_else(|| io_error("file closed")))
+        .await?
+        .map_err(|_| Error::from(io_error("blocking task failed")))
+        .await?
+    }
+}
+
+impl AsyncWrite for AsyncWriter {
+    fn poll_write(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<std::io::Result<usize>> {
+        let state = &mut *self.0.lock().unwrap();
+
+        loop {
+            match state {
+                State::Idle(opt) => {
+                    // Grab a reference to the inner representation of the file or return an error
+                    // if the file is closed.
+                    let inner = opt.as_mut().ok_or_else(|| io_error("file closed"))?;
+
+                    // Check if the operation has completed.
+                    if let Some(Operation::Write(res)) = inner.last_op.take() {
+                        let n = res?;
+
+                        // If more data was written than is available in the buffer, let's retry
+                        // the write operation.
+                        if n <= buf.len() {
+                            return Poll::Ready(Ok(n));
+                        }
+                    } else {
+                        let mut inner = opt.take().unwrap();
+
+                        // Set the length of the inner buffer to the length of the provided buffer.
+                        if inner.buf.len() < buf.len() {
+                            inner.buf.reserve(buf.len() - inner.buf.len());
+                        }
+                        unsafe {
+                            inner.buf.set_len(buf.len());
+                        }
+
+                        // Copy the data to write into the inner buffer.
+                        inner.buf[..buf.len()].copy_from_slice(buf);
+
+                        // Start the operation asynchronously.
+                        *state = State::Busy(spawn_blocking(async move {
+                            let res = std::io::Write::write(&mut inner.tmpfile, &inner.buf);
+                            inner.last_op = Some(Operation::Write(res));
+                            State::Idle(Some(inner))
+                        }));
+                    }
+                }
+                // Poll the asynchronous operation the file is currently blocked on.
+                State::Busy(task) => *state = futures::ready!(Pin::new(task).poll(cx)),
+            }
+        }
+    }
+
+    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
+        let state = &mut *self.0.lock().unwrap();
+
+        loop {
+            match state {
+                State::Idle(opt) => {
+                    // Grab a reference to the inner representation of the file or return if the
+                    // file is closed.
+                    let inner = match opt.as_mut() {
+                        None => return Poll::Ready(Ok(())),
+                        Some(s) => s,
+                    };
+
+                    // Check if the operation has completed.
+                    if let Some(Operation::Flush(res)) = inner.last_op.take() {
+                        return Poll::Ready(res);
+                    } else {
+                        let mut inner = opt.take().unwrap();
+
+                        // Start the operation asynchronously.
+                        *state = State::Busy(spawn_blocking(async move {
+                            let res = std::io::Write::flush(&mut inner.tmpfile);
+                            inner.last_op = Some(Operation::Flush(res));
+                            State::Idle(Some(inner))
+                        }));
+                    }
+                }
+                // Poll the asynchronous operation the file is currently blocked on.
+                State::Busy(task) => *state = futures::ready!(Pin::new(task).poll(cx)),
+            }
+        }
+    }
+
+    fn poll_close(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
+        let state = &mut *self.0.lock().unwrap();
+
+        loop {
+            match state {
+                State::Idle(opt) => {
+                    // Grab a reference to the inner representation of the file or return if the
+                    // file is closed.
+                    let inner = match opt.take() {
+                        None => return Poll::Ready(Ok(())),
+                        Some(s) => s,
+                    };
+
+                    // Start the operation asynchronously.
+                    *state = State::Busy(spawn_blocking(async move {
+                        drop(inner);
+                        State::Idle(None)
+                    }));
+                }
+                // Poll the asynchronous operation the file is currently blocked on.
+                State::Busy(task) => *state = futures::ready!(Pin::new(task).poll(cx)),
+            }
+        }
+    }
+}
+
+fn io_error(err: impl Into<Box<dyn std::error::Error + Send + Sync>>) -> std::io::Error {
+    std::io::Error::new(std::io::ErrorKind::Other, err)
 }
 
 #[cfg(test)]
